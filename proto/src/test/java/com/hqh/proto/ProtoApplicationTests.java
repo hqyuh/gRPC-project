b504ed603c8420ec6ebbf4673067adb992c10e40
hqyuh <hoquanghuy0123@gmail.com>
2023-06-17 21:09:13 +0700

    project initialization

---
+++ proto/src/test/java/com/hqh/proto/ProtoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hqh.proto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
